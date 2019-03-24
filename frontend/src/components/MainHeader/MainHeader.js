7248ded23dd4d7054f04c1705d1f2359eab77473
Biraj Adhikari <birajadhikari@Birajs-MacBook-Pro.local>
2019-03-24 09:58:05 -0400

    Create main public header or appbar

--- frontend/src/components/MainHeader/MainHeader.js
+++ frontend/src/components/MainHeader/MainHeader.js
@@ -1,78 +1,54 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
-import MenuIcon from "@material-ui/icons/Menu";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import PersonIcon from "@material-ui/icons/Person";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = theme => ({
-  toolbarRoot: {
-    paddingRight: 24
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36
+  root: {
+    flexGrow: 1
   },
   title: {
-    flexGrow: 1
-  }
+    flexGrow: 1,
+    marginLeft: 12,
+    marginRight: 36,
+    fontFamily: ("Times New Roman", "Times", "serif")
+  },
+  navigation: {
+    display: "flex",
+    flexDirection: "row",
+    padding: "2px",
+  },
+
 });
 
 const MainHeader = props => {
 
-  const { classes, handleToggleDrawer } = props;
+  const { classes } = props;
 
   return (
+    <div className={classes.root}>
       <AppBar position="fixed">
-        <Toolbar disableGutters={true} classes={{ root: classes.toolbarRoot }}>
-
-          {/* MENU TOGGLE */}
-          <IconButton
-            color="inherit"
-            aria-label="Open drawer"
-            onClick={handleToggleDrawer}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-
+        <Toolbar disableGutters={true}>
           {/* TITLE */}
-          <Typography
-            variant="title"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            Dashboard
+          <Typography variant="title" color="inherit" className={classes.title}>
+            LIBRE FOOD PANTRY
           </Typography>
 
-          {/* NOTIFICATIONS */}
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <Menu
-            id="simple-menu"
-          >
-            <MenuItem >Profile</MenuItem>
-            <MenuItem >My account</MenuItem>
-            <MenuItem >Logout</MenuItem>
-          </Menu>
-
-          {/* PROFILE */}
-          <IconButton onClick={props.logout} color="inherit">
-            <PersonIcon />
-          </IconButton>
+          {/* Navigation */}
+          <div className={classes.navigation}>
+            <Button variant="h8" color="inherit" >
+              HOW IT WORKS
+            </Button>
+            <Button variant="h8" color="inherit" >
+              ABOUT
+            </Button>
+          </div>
 
         </Toolbar>
       </AppBar>
+    </div>
   );
 };
 
--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -1,5 +1,64 @@
-import React from 'react'
+import React, { Fragment, Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
+import { connect } from "react-redux";
 
-const MainLayout = props => <div style={{ height: 'inherit' }}>{props.children}</div>
+import MainHeader from "../components/MainHeader";
 
-export default MainLayout
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    marginLeft: theme.spacing.unit * 9,
+    padding: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 7,
+    overflowX: "hidden"
+  },
+  contentShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  }
+});
+
+class MainLayout extends Component {
+
+  state =  {
+    open: false
+  };
+
+  handleToggleDrawer = () => {
+    this.setState(prevState => {
+      return { open: !prevState.open };
+    });
+  };
+
+  render() {
+    const { classes, children } = this.props;
+    return (
+      <Fragment>
+        <div className={classes.root}>
+          <MainHeader />
+          <main className={classNames(classes.content, {[classes.contentShift]: this.state.open})}>
+            {children}
+          </main>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(withStyles(styles)(MainLayout));
