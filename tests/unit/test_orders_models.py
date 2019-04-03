265d8a7388bb0d25e891453523c43c9ebfbcb2cc
Nick <ndion617@gmail.com>
2019-04-03 15:11:42 -0400

    Part of #5

---
+++ tests/unit/test_orders_models.py
@@ -0,0 +1,20 @@
+"""
+This file contains the unit tests for the orders model
+"""
+
+#Add a new order to the DB
+def test_new_order():
+    #create an order
+    #verify has entered into database
+    return
+
+#Tests getting an order by email
+def test_find_email():
+    #Given an known order 
+    #check returns a dictronary?
+    return
+
+def test_find_id():
+    #given a known order
+    #check that reutrns a dictornary?
+    return
