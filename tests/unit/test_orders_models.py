a86c821f0a88bfe41f00f3c3e7506802a761e1bf
Nick <ndion617@gmail.com>
2019-04-08 15:08:46 -0400

    Adjusted tests to use fixture.

--- tests/unit/test_orders_models.py
+++ tests/unit/test_orders_models.py
@@ -2,7 +2,7 @@
 This file contains the unit tests for the orders model
 """
 
-def test_new_order():
+def test_new_order(new_order):
     """
     GIVEN a Order model
     WHEN a new Order is created
@@ -13,26 +13,26 @@ def test_new_order():
     assert new_order.created_on != None
 
 
-def test_find_email():
+def test_find_email(new_order):
     """
     GIVEN a User Model
     WHEN the Email of the user is defined to a value
     THEN check the Email returns a INT
     """
     new_order.email = "example@email.com"
-    assert isinstance(new_order.find_by_email(), str)
-    assert not isinstance(new_order.find_by_email(), int)
-    assert new_order.find_by_email() == "example@email.com"
+    assert isinstance(new_order.find_by_email("example@email.com"), str)
+    assert not isinstance(new_order.find_by_email("example@email.com"), int)
+    assert new_order.find_by_email("example@email.com") == "1"
     
 
-def test_find_id():
+def test_find_id(new_order):
     """
     GIVEN an existing order
     WHEN the ID of the user is defined to a value
     THEN check the user ID returns a string (and not an integer) as needed by Flask-WTF
     """
-    new_user.id = 42
-    assert isinstance(new_order.find_by_id(), str)
-    assert not isinstance(new_order.find_by_id(), int)
+    new_order.id = 42
+    assert isinstance(new_order.find_by_id(42), str)
+    assert not isinstance(new_order.find_by_id(42), int)
     assert new_order.find_by_id() == "42"
     
