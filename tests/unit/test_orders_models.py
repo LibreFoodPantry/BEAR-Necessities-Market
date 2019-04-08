836d878f0be3eca4999303b3864bdf3c2fd6570a
Nick <ndion617@gmail.com>
2019-04-08 11:21:37 -0400

    added tests to validate order model.

--- tests/unit/test_orders_models.py
+++ tests/unit/test_orders_models.py
@@ -2,19 +2,37 @@
 This file contains the unit tests for the orders model
 """
 
-#Add a new order to the DB
 def test_new_order():
-    #create an order
-    #verify has entered into database
-    return
+    """
+    GIVEN a Order model
+    WHEN a new Order is created
+    THEN check has email 
+    """
+    assert new_order.email == "test@email.com"
+    assert new_order.id != None
+    assert new_order.created_on != None
 
-#Tests getting an order by email
+
 def test_find_email():
-    #Given an known order 
-    #check returns a dictronary?
-    return
+    """
+    GIVEN a User Model
+    WHEN the Email of the user is defined to a value
+    THEN check the Email returns a INT
+    """
+    new_order.email = "example@email.com"
+    assert isinstance(new_order.find_by_email(), str)
+    assert not isinstance(new_order.find_by_email(), int)
+    assert new_order.find_by_email() == "example@email.com"
+    
 
 def test_find_id():
-    #given a known order
-    #check that reutrns a dictornary?
-    return
+    """
+    GIVEN an existing order
+    WHEN the ID of the user is defined to a value
+    THEN check the user ID returns a string (and not an integer) as needed by Flask-WTF
+    """
+    new_user.id = 42
+    assert isinstance(new_order.find_by_id(), str)
+    assert not isinstance(new_order.find_by_id(), int)
+    assert new_order.find_by_id() == "42"
+    
