10a66f25e8768d1fb9a8a4aa7e3a83ebbe85dad9
Ava Rule <afrule58@gmail.com>
2019-04-10 14:44:31 -0400

    Add method to send mail using Flask-SendGrid

--- backend/utils/mail.py
+++ backend/utils/mail.py
@@ -1,12 +1,18 @@
 from flask import current_app, render_template
 from flask_mail import Message
+from flask_sendgrid import SendGrid
 
 
-def send_mail(subject: str, recipients: list, template, sender=None, **ctx):
-    if not isinstance(recipients, (tuple, list)):
-        recipients = [recipients]
+def send_email(from_email='someone@yourdomain.com',to_email='test@example.com',
+    subject='Subject', text = 'Body'):
+    #if not isinstance(to_email, (tuple, list)):
+        #to_email = [to_email]
 
-    msg = Message(subject=subject, recipients=recipients, sender=sender)
-    msg.html = render_template(template, **ctx)
+    #msg = Message(subject=subject, recipients=to_email, sender=from_email)
+    #msg.html = render_template(template, **ctx)
 
-    msg.send('')
+    #msg.send('')
+
+
+
+#subject: str, recipients: list, template, sender=None, **ctx
